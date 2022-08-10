35ec4c1c06973a2c617dfff0719f0ea17b054aee
Michael Bestas <mkbestas@lineageos.org>
2022-08-10 05:18:59 +0300

    kernel: Allow overriding dtbo target name
    gs101 kernel uses google/dtbo.img target for dtbo.img
    
    Change-Id: Ib03f35c0035f56d303771de9399bcce3d02c0775

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -452,7 +452,7 @@ else
 endif # BOARD_KERNEL_PAGESIZE
 endif # BOARD_DTBO_CFG
 else
-	$(call make-dtbo-target,dtbo.img)
+	$(call make-dtbo-target,$(TARGET_KERNEL_DTBO))
 endif # BOARD_KERNEL_SEPARATED_DTBO
 	$(hide) touch -c $(DTBO_OUT)
 endif # BOARD_CUSTOM_DTBOIMG_MK
